3f6b85586947067fdb6cbb4503d0e4f177d78dfa
Alamin-Rintu <alaminrintu01@gmail.com>
2026-03-30 01:47:42 +0600

    added newsletter section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
+import NewsLetterSection from "./components/NewsLetterSection/NewsLetterSection";
 import PricingSection from "./components/PricingSection/PricingSection";
 import Stats from "./components/Stats/Stats";
 import StepSection from "./components/StepSection/StepSection";
@@ -8,11 +9,12 @@ import StepSection from "./components/StepSection/StepSection";
 function App() {
   return (
     <>
-      <Navbar/>
-      <Banner/>
-      <Stats/>
-      <StepSection/>
-      <PricingSection/>
+      <Navbar />
+      <Banner />
+      <Stats />
+      <StepSection />
+      <PricingSection />
+      <NewsLetterSection />
     </>
   );
 }
---
+++ src/components/NewsLetterSection/NewsLetterSection.jsx
@@ -0,0 +1,26 @@
+const NewsLetterSection = () => {
+  return (
+    <div className="bg-linear-65 from-[#4F39F6] to-[#9514FA] p-10 text-center mt-16 mb-16">
+      <h3 className="text-5xl font-bold text-white">
+        Ready to Transform Your Workflow?
+      </h3>
+      <p className="text-sm text-white mt-3 mb-3">
+        Join thousands of professionals who are already using Digitools to work
+        smarter. <br /> Start your free trial today.
+      </p>
+      <div className="">
+        <button className="btn rounded-full text-[#9514FA] mr-4">
+          Explore Products
+        </button>
+        <button className="btn rounded-full bg-transparent text-white">
+          View Pricing
+        </button>
+      </div>
+      <p className="text-white text-sm mt-3">
+        14-day free trial • No credit card required • Cancel anytime
+      </p>
+    </div>
+  );
+};
+
+export default NewsLetterSection;
