8d8545f22310cfaff3b8495879fcd45e14b80b25
Alamin-Rintu <alaminrintu01@gmail.com>
2026-03-29 20:43:39 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
-
   return (
     <>
-      <h3 className='text-4xl text-center mt-10'>This is rintu.</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, maxime ut. Aperiam natus, nulla eius odio deleniti sint delectus libero veniam corrupti iure. Sed expedita culpa, libero dolor voluptatibus dicta reiciendis inventore atque ex, repellat assumenda. Dolor quaerat vel dolorum facilis illum quisquam accusantium non impedit, eos iste! Dolore itaque, voluptas odio dicta blanditiis earum nostrum ut officiis ipsum fugiat vel possimus optio molestiae nam quasi modi laboriosam recusandae, aut repudiandae eum quibusdam quaerat. Blanditiis praesentium voluptatem fuga voluptatum similique facere enim obcaecati laborum quas, aliquam distinctio itaque, aspernatur illum earum modi rerum deserunt neque eum sint inventore incidunt totam!</p>
+      <Navbar/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
